23aab9eb3590fe462c4a5a14cc626fd6f4f59baa
Mujib517 <mujib517@gmail.com>
2017-05-20 19:55:00 +0530

    bootstrapping

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 @NgModule({
-    declarations: [AppComponent]
+    imports:[BrowserModule],
+    declarations: [AppComponent],
+    bootstrap:[AppComponent]
 })
 export class AppModule { }
