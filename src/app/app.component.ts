365ff54db0cd1f3fedaf1e951c290e1c218ddd2b
Mujib517 <mujib517@gmail.com>
2017-05-21 18:12:17 +0530

    Templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: '<h1>Hello Angular !!</h1>'
+    templateUrl: './app.html'
 })
 export class AppComponent { }
