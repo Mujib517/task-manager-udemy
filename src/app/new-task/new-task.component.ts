e167996865e69c57a8f89535237190c8b799b570
Mujib517 <mujib517@gmail.com>
2017-08-13 11:52:06 +0530

    Posting form

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from "app/shared/task.model";
+import { TaskService } from "app/shared/task.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
   styles: []
 })
-export class NewTaskComponent implements OnInit {
+export class NewTaskComponent {
 
-  constructor() { }
+  task: Task;
 
-  ngOnInit() {
+  constructor(private taskSvc: TaskService,private router:Router) {
+    this.task = new Task();
+    
+    //generate a random number betweeen 1 and 100
+    this.task.id=Math.floor((Math.random() * 100) + 1);
+  }
+
+  onSave() {
+    this.taskSvc.save(this.task)
+      .subscribe((response) => this.router.navigate(['/tasks']));
   }
 
 }
--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -1,6 +1,7 @@
 import { Http, Response } from '@angular/http';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs/Observable';
+import { Task } from "app/shared/task.model";
 
 @Injectable()
 export class TaskService {
@@ -9,5 +10,9 @@ export class TaskService {
 
     get(): Observable<Response> {
         return this.http.get('http://localhost:3000/tasks');
+    }
+
+    save(task: Task): Observable<Response> {
+        return this.http.post('http://localhost:3000/tasks', task);
     }
 }
